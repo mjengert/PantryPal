c34420c42cdf599c486909a2714f3be03a5614ef
Conner Layne <112344329+cjlayne@users.noreply.github.com>
2025-04-01 17:07:16 -0400

    Add files via upload

---
+++ item.py
@@ -0,0 +1,67 @@
+
+from datetime import datetime, timedelta
+
+# Dictionary mapping categories to expiration times
+food_expiration = {
+    "Meat": ["beef", "pork", "lamb", "veal", "steak", "sirloin", "ribeye", "pork chops"],
+    "Ground Meat": ["ground beef", "ground pork", "ground chicken", "ground turkey"],
+    "Poultry": ["chicken", "turkey", "duck", "chicken thighs", "turkey leg", "chicken breast", "chicken wings"],
+    "Fish": ["fish", "salmon", "tuna", "cod", "tilapia", "catfish"],
+    "Cooked Meats": ["cooked beef", "cooked chicken", "cooked pork", "cooked turkey","lunch meat", "ham", "roast beef", "turkey slices", "deli meat", "sliced meat",
+                     "sliced chicken", "deli chicken", "deli turkey"],
+    "Eggs": ["eggs"],
+    "Milk": ["milk", "butter", "yogurt", "cream", "half and half","cheese"],
+    "Fruits": [
+        "banana", "apple", "grape", "strawberry", "blueberry", "raspberry", 
+        "orange", "avocado", "melon", "pear", "lemon", "lime", "peach", "pineapple"
+    ],
+    "Vegetables": [
+        "potato", "tomato", "lettuce", "carrot", "onion", "cucumber", 
+        "pepper", "broccoli", "celery", "spinach", "squash"
+    ],"Grains": ["rice", "pasta", "oats", "quinoa", "barley", "cereal"],
+    "Bread": ["bread"]
+}
+
+expiration_times = {
+    "Meat": 5,
+    "Ground Meat": 2,
+    "Poultry": 2,
+    "Fish": 2,
+    "Cooked Meats": 5,
+    "Eggs": 35,  
+    "Milk": 7,
+    "Fruits": 7,  
+    "Vegetables": 7,
+    "Grains": 180,
+    "Bread": 7
+}
+class Item:
+    def __init__(self, name):
+        self.name = name.lower()
+        self.itemType = self.getType()
+        self.expiration = ""
+    
+    #Get grocery type from list
+    def getType(self):
+        itemCat="Unknown"
+        for category, items in food_expiration.items():
+            for item in items:
+                if item in self.name:
+                    itemCat = category
+
+        return itemCat
+    
+    def getName(self):
+        return self.name
+    
+    #return expiration date
+    def getExpiration(self):
+        return self.expiration
+    
+    #Get expiration date from list   
+    def calcExpiration(self):
+        if self.itemType in expiration_times:
+            days_until_expire = expiration_times[self.itemType]
+            expiration_date = datetime.today() + timedelta(days=days_until_expire)
+            self.expiration = expiration_date.strftime("%Y-%m-%d")
+        return "N/A"
