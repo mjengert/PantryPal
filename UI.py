343acd5d5a100b16856fd493996bc49fd36fafda
mjengert <m.engert@ufl.edu>
2025-04-17 20:28:15 -0500

    work on recipe screen

--- UI.py
+++ UI.py
@@ -1,8 +1,13 @@
 from kivy.metrics import dp
-from kivy.properties import StringProperty, ListProperty
+from kivy.properties import StringProperty, ListProperty, Clock
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.appbar import MDTopAppBar, MDTopAppBarLeadingButtonContainer, MDActionTopAppBarButton, MDTopAppBarTitle
+from kivymd.uix.button import MDButton, MDButtonIcon, MDButtonText, MDIconButton
+from kivymd.uix.card import MDCard
 
 from kivymd.uix.fitimage import FitImage
+from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.boxlayout import MDBoxLayout
@@ -15,15 +20,18 @@ from kivymd.uix.navigationbar.navigationbar import (
 from kivymd.app import MDApp
 
 from kivymd.uix.list import MDListItem, MDListItemHeadlineText, MDList, MDListItemTrailingCheckbox, \
-    MDListItemSupportingText, MDListItemTrailingIcon
+    MDListItemSupportingText, MDListItemTrailingIcon, MDListItemLeadingAvatar, MDListItemTertiaryText
 from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
+from kivymd.uix.scrollview import MDScrollView
 from kivymd.uix.selectioncontrol import MDCheckbox
+from kivymd.uix.stacklayout import MDStackLayout
 from kivymd.uix.textfield import MDTextField, MDTextFieldHintText
 
 from item import Item
 from groceryList import Grocery_List
 from pantryList import Pantry_List
+from recipeGenerator import RecipeGenerator
 from UserData import *
 
 #****************** Pulling Data from Database *********************#
@@ -62,6 +70,270 @@ class BaseScreen(MDScreen):
                 radius=dp(24),
             ),
         )
+
+################################ RECIPE GENERATOR SCREEN ####################################
+
+class RecipeGenScreen(MDScreen):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.addedMissing = False
+
+        # creates a layout for the list to be added to
+        layout = MDBoxLayout(
+            orientation="vertical",
+            spacing=10,
+            padding=dp(10),
+        )
+        self.add_widget(layout)
+
+        # creates search bar
+        searchBar = MDTextField(
+            MDTextFieldHintText(text="Search for recipe"),
+            size_hint_x=0.8,
+            pos_hint={"center_x": 0.5, "center_y": 0.9},
+        )
+        layout.add_widget(searchBar)
+
+        # adds scroll functionality to the screen
+        scroll = MDScrollView()
+        layout.add_widget(scroll)
+
+        # creates a list to hold the recipes and add it to the scroll view
+        md_list = MDList(
+            pos_hint={'center_y': 0.7},
+            size_hint=(1, None),
+            height=dp(400),
+        )
+        scroll.add_widget(md_list)
+
+        # calling the recipe generator API
+        recipeGen = RecipeGenerator()
+
+        # get the ingredients from the pantry
+        ingredients = []
+        for i in range(pantry_list.getRange()):
+            ingredients.append(pantry_list.getItem(i).getName())
+
+        # get the recipes based on the ingredients
+        recipeGen.generateRecipe(ingredients)
+
+        # get recipe info from the API
+        for recipe in recipeGen.recipeList:
+            recipeGen.getRecipeInfo(recipe)
+
+        # if pantry list is empty, show default recipes
+        if recipeGen.recipeList == []:
+            md_list.add_widget(MDListItem(text='No recipes found'))
+
+        # if pantry list is not empty, show recipes based on pantry items
+        else:
+            for recipe in recipeGen.recipeList:
+                # creates a list item for each recipe
+                # display the recipe name
+                md_list_item = MDListItem(
+                    # display the recipe name
+                    MDListItemHeadlineText(
+                        text=recipe.getName()
+                    ),
+                    # display the recipe image
+                    MDListItemLeadingAvatar(
+                        source=recipe.getImage()
+                    ),
+                    # display the recipe ingredients owned
+                    MDListItemSupportingText(
+                        text="Owned Ingredients: " + recipe.ownedIngredients()
+                    ),
+                    # display the recipe ingredients missing
+                    MDListItemTertiaryText(
+                        text="Missing Ingredients: " + recipe.missingIngredients()
+                    ),
+                )
+
+                # adds button to automatically add missing ingredients to grocery list
+                addMissingButton = MDIconButton(
+                    icon="plus",
+                    style="filled",
+                    md_bg_color=self.theme_cls.secondaryColor,
+                    pos_hint={"center_x": 0.5, "center_y": 0.5},
+                )
+                # binds the buttons to their functions
+                addMissingButton.bind(on_release=lambda instance, r=recipe: self.missingIngredients(instance, r))
+                md_list_item.add_widget(addMissingButton)
+                md_list_item.bind(on_release=lambda instance, r=recipe: self.recipeInfo(r))
+                md_list.add_widget(md_list_item)
+
+    # function to display the recipe information when the recipe is clicked
+    def recipeInfo(self, recipe):
+        # check if recipe screen already exists
+        if self.manager.has_screen("Recipe Info"):
+            self.manager.remove_widget(self.manager.get_screen("Recipe Info"))
+
+        # creates a new screen to display the recipe information
+        recipeInfoScreen = RecipeInfoScreen(name="Recipe Info", recipe=recipe)
+        self.manager.add_widget(recipeInfoScreen)
+        self.manager.transition.direction = "left"
+        self.manager.current = "Recipe Info"
+
+    # function to add or remove missing ingredients from grocery list
+    def missingIngredients(self, instance, recipe):
+        # check if the ingredients were already added to the grocery list; if so, remove them
+        if self.addedMissing:
+            self.deleteMissingIngredients(instance, recipe)
+
+        # otherwise, add them to the grocery list
+        else:
+            self.addMissingIngredients(instance, recipe)
+
+    # function to add missing ingredients to grocery list, toggle icon, and announcement
+    def addMissingIngredients(self, instance, recipe):
+        self.toggleIcon(instance)
+        self.toggleAnnouncment("Missing ingredients added to grocery list")
+        self.addedMissing = True
+
+    # function to remove missing ingredients from grocery list, toggle icon, and announcement
+    def deleteMissingIngredients(self, instance, recipe):
+        self.toggleIcon(instance)
+        self.toggleAnnouncment("Missing ingredients removed from grocery list")
+        self.addedMissing = False
+
+    # function to toggle the icon of the button when pressed
+    def toggleIcon(self, instance):
+        if instance.icon == "plus":
+            instance.icon = "check"
+        else:
+            instance.icon = "plus"
+
+    # function to toggle the announcement message when button is pressed
+    def toggleAnnouncment(self, announcement):
+        # remove the previous announcement if it exists
+        if hasattr(self, announcement):
+            self.remove_widget(announcement)
+
+        # creates announcement message layout
+        self.layout = MDBoxLayout(
+            orientation="vertical",
+            size_hint=(0.3, None),
+            height=dp(50),
+            padding=dp(10),
+            md_bg_color=self.theme_cls.primaryColor,
+            pos_hint={"center_x": 0.5, "top": 0.95},
+        )
+
+        # creates the announcement message label
+        self.announcement = MDLabel(
+            text=announcement,
+            halign="center",
+            valign="middle",
+            theme_text_color="Custom",
+            text_color=(1, 1, 1, 1),
+        )
+
+        # adds the announcement message to the layout and sets timer to remove it
+        self.layout.add_widget(self.announcement)
+        self.add_widget(self.layout)
+        Clock.schedule_once(lambda dt: self.removeAnnouncement(), 3)
+
+    # function to remove the announcement message after 3 seconds
+    def removeAnnouncement(self):
+        # if the announcement exists, remove it and its layout
+        if hasattr(self, "layout"):
+            self.remove_widget(self.layout)
+            self.layout = None
+        if hasattr(self, "announcement"):
+            self.remove_widget(self.announcement)
+            self.announcement = None
+
+
+############################### RECIPE INFO SCREEN ####################################
+class RecipeInfoScreen(MDScreen):
+
+    def __init__(self, recipe, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # creates a layout for the recipe info screen
+        layout = MDBoxLayout(
+            orientation="vertical",
+            spacing=10,
+            padding=dp(10),
+        )
+        self.add_widget(layout)
+
+        # adds top bar to the screen
+        topBar = MDTopAppBar(
+            MDTopAppBarLeadingButtonContainer(
+                MDActionTopAppBarButton(
+                    icon="arrow-left",
+                    on_release=self.go_back,
+                ),
+                MDTopAppBarTitle(
+                    text="Back"
+                )
+            ),
+            md_bg_color=self.theme_cls.primaryColor,
+        )
+        layout.add_widget(topBar)
+
+        # adds scroll functionality to the screen
+        scroll = MDScrollView()
+        layout.add_widget(scroll)
+
+        # creates layout for the recipe info section
+        recipeInfoLayout = MDBoxLayout(
+            orientation="vertical",
+            adaptive_height=True,
+        )
+        recipeInfoLayout.bind(minimum_height=recipeInfoLayout.setter('height'))
+        scroll.add_widget(recipeInfoLayout)
+
+        # creates a label to display the recipe name
+        recipName = MDLabel(
+            text=recipe.getName(),
+            halign="center",
+            padding=dp(10),
+            adaptive_height=True,
+        )
+        recipeInfoLayout.add_widget(recipName)
+
+        # display the recipe image
+        recipeImage = FitImage(
+            source=recipe.getImage(),
+            size_hint_y=None,
+            height=dp(400),
+            radius=dp(24),
+        )
+        recipeInfoLayout.add_widget(recipeImage)
+
+        # creates a label to display the recipe summary
+        summaryLabel = MDLabel(
+            text="Summary: " + recipe.getSummary(),
+            markup=True,
+            padding=dp(10),
+            adaptive_height=True,
+        )
+        recipeInfoLayout.add_widget(summaryLabel)
+
+        # display the recipe ingredients owned and missing
+        # missing ingredients displayed in red
+        ingredients = (
+                "[color=00C853]" + ", ".join(recipe.ownedIngredients()) + "[/color]\n"
+                                                                          "[color=F44336]" + ", ".join(
+            recipe.missingIngredients()) + "[/color]"
+        )
+        # creates a label to display the recipe ingredients
+        ingredientsLabel = MDLabel(
+            text="Ingredients: " + ingredients,
+            markup=True,
+            padding=dp(10),
+            adaptive_height=True,
+        )
+        recipeInfoLayout.add_widget(ingredientsLabel)
+
+
+    def go_back(self, *args):
+        self.manager.transition.direction = "right"
+        self.manager.current = "Recipe Generator"
+
 
 ############################### PANTRY LIST SCREEN ####################################
 class PantryListScreen(MDScreen):
@@ -169,16 +441,15 @@ class PantryPalUI(MDApp):
         return MDBoxLayout(
             MDScreenManager(
                 BaseScreen(
+                    name="Home",
+                    image_size="1024",
+                ),
+                RecipeGenScreen(
                     name="Recipe Generator",
-                    image_size="700"
                 ),
                 PantryListScreen(
                     name="My Pantry",
                     #image_size="600",
-                ),
-                BaseScreen(
-                    name="Home",
-                    image_size="1024",
                 ),
                 GroceryListScreen(
                     name="Grocery List",
--- recipeGenerator.py
+++ recipeGenerator.py
@@ -12,21 +12,126 @@ configuration = spoonacular.Configuration(
 # setting up the API key
 configuration.api_key['apiKeyScheme'] = os.getenv("SPOONACULAR_API_KEY")
 
+# This is the class that will be used to generate recipes based on the ingredients in the pantry
 class RecipeGenerator:
+    # initializing the recipe generator object
     def __init__(self):
         self.api_instance = spoonacular.RecipesApi(spoonacular.ApiClient(configuration))
+        self.recipeList = []
 
-    # function returns top 5 recipes based on the ingredients provided
-    def generateRecipe(self, ingredients):
+    # function returns top 5 recipes based on the ingredients in the pantry
+    def generateRecipe(self, ingredientsList):
+        if len(ingredientsList) == 0:
+            print("No ingredients found in the pantry.")
+            return []
         try:
+            # format ingredients into a string for the API
+            ingredientsStr = ''
+            for i in range(len(ingredientsList)):
+                ingredientsStr += ingredientsList[i]
+                # if not the last ingredient, add a comma
+                if i != len(ingredientsList) - 1:
+                    ingredientsStr += ','
             # calling the API to get recipes based on the ingredients
             api_response = self.api_instance.search_recipes_by_ingredients(
-                ingredients=ingredients,
-                number=5,
+                ingredients=ingredientsStr,
+                number=7,
                 ranking=2,
                 ignore_pantry=False
             )
-            return api_response
+            # if recipe gen is currntly empty, add the recipes to the list
+            if len(self.recipeList) == 0:
+                for recipe in api_response:
+                    self.recipeList.append(Recipe(recipe))
+            else:
+                # if recipe gen is not empty, clear the list and add the new recipes
+                self.recipeList.clear()
+                for recipe in api_response:
+                    self.recipeList.append(Recipe(recipe))
+            print("Recipes generated based on the ingredients in the pantry.")
         except ApiException as e:
-            print("Exception when calling RecipeApi->generate_recipes_by_ingredients: %s\n" % e)
+            print("Exception when calling RecipeApi->search_recipes_by_ingredients: %s\n" % e)
+
+    # function to get the recipe list
+    def getRecipeList(self):
+        return self.recipeList
+
+    # function to get recipe by id
+    def getRecipeById(self, recipeId):
+        for recipe in self.recipeList:
+            if recipe.id == recipeId:
+                return recipe
+        return None
+
+    # function to get recipe by name
+    def getRecipeByName(self, recipeTitle):
+        for recipe in self.recipeList:
+            if recipe.title == recipeTitle:
+                return recipe
+        return None
+
+    # function to get more information about the recipe
+    def getRecipeInfo(self, recipe):
+        if recipe is not None:
+            api_response = self.api_instance.get_recipe_information(recipe.id)
+            recipe.servings = api_response.servings
+            recipe.cooking_time = api_response.ready_in_minutes
+            recipe.summary = api_response.summary
+            recipe.source_url = api_response.source_url
+        else:
+            return None
+
+# This is the class that will be used to store the recipe information
+class Recipe:
+    # initializing the recipe object
+    def __init__(self, recipe):
+        self.id = recipe.id
+        self.title = recipe.title
+        self.image = recipe.image
+        self.missed_ingredients = ', '.join(
+            [ing.name for ing in recipe.missed_ingredients if ing.name]
+        )
+        self.used_ingredients = ', '.join(
+            [ing.name for ing in recipe.used_ingredients if ing.name]
+        )
+        self.servings = 0
+        self.cooking_time = 0
+        self.summary = ''
+        self.source_url = ''
+
+    # function to get the recipe name
+    def getName(self):
+        return self.title
+
+    # function to get the recipe image
+    def getImage(self):
+        return self.image
+
+    # function to get the recipe id
+    def ownedIngredients(self):
+        return self.used_ingredients
+
+    # function to get the recipe id
+    def missingIngredients(self):
+        return self.missed_ingredients
+
+    # function to get the recipe id
+    def getId(self):
+        return self.id
+
+    # function to get the recipe servings
+    def getServings(self):
+        return self.servings
+
+    # function to get the recipe cooking time
+    def getCookingTime(self):
+        return self.cooking_time
+
+    # function to get the recipe summary
+    def getSummary(self):
+        return self.summary
+
+    # function to get the recipe source url
+    def getSourceUrl(self):
+        return self.source_url
 
